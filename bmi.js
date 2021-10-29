11b46f8ed2177590ab7da5f00a86bd980d34db12
rashedul24 <ronyuap@gmail.com>
2021-10-30 01:50:32 +0600

    calculation added

--- bmi.js
+++ bmi.js
@@ -7,9 +7,17 @@ const bmiCal = () => {
   
   const heightInput = document.getElementById('height');
   const height = heightInput.value;
-
+  // clear input field
   weightInput.value = '';
   heightInput.value = '';
-  
+  // calculation
   const calculation = weight / (height * height);
+  const result = document.getElementById('result');
+  const div = document.createElement('div');
+  result.innerText = '';
+  // output
+  div.innerHTML = ` 
+  <h2>Your BMI is ${calculation}</h2>
+  `
+  result.appendChild(div);
 }
