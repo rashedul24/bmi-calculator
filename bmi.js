363f2b6295659454e71f6e9c4ad7ca672b513a6e
rashedul24 <ronyuap@gmail.com>
2021-10-30 01:43:45 +0600

    value

--- bmi.js
+++ bmi.js
@@ -1,3 +1,15 @@
 const calculate = document.getElementById('calculate');
 
 
+const bmiCal = () => {
+  const weightInput = document.getElementById('weight');
+  const weight = weightInput.value;
+  
+  const heightInput = document.getElementById('height');
+  const height = heightInput.value;
+
+  weightInput.value = '';
+  heightInput.value = '';
+  
+  const calculation = weight / (height * height);
+}
